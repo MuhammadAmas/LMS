d2eca3cbb240547dba25a9b1e0f9fbf9574c9cf1
muhammadamas <amaswaseem@gmail.com>
2023-03-26 01:47:16 +0500

    Added SignUp, SignIn Components and Routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import { CssBaseline } from '@mui/material';
-import { Routes, Route } from 'react-router-dom';
-import { Student, Teacher, Lms, NavBar, Profile } from './Components';
-import './App.css'
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import {
+  Student,
+  Teacher,
+  Lms,
+  NavBar,
+  Profile,
+  SignUp,
+  LogIn,
+} from "./Components";
+import "./App.css";
+// import { SignUp } from './Components/SignUp/SignUp';
 
 export default function App() {
   return (
     <div className="#">
       <CssBaseline />
       <NavBar />
+
+      <LogIn />
       <main className="#">
         <div className="#" />
         <Routes>
+          <Route path="/SignUp" element={<SignUp />}/>
           <Route exact path="/student/:id" element={<Student />} />
           <Route exact path="/teacher/:id" element={<Teacher />} />
           <Route exact path="/" element={<Lms />} />
--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -3,3 +3,5 @@ export { default as Lms } from './Lms/Lms.jsx';
 export { default as Teacher } from './Teacher/Teacher.jsx';
 export { default as NavBar } from './NavBar/NavBar.jsx';
 export { default as Profile } from './Profile/Profile.jsx';
+export { default as SignUp } from './SignUp/SignUp.jsx';
+export { default as LogIn } from './LogIn/LogIn.jsx';
