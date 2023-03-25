0e011d0497b8d1cb531b574583a0c12018822f27
ZainAsif767 <zainclasher2002@gmail.com>
2023-03-25 18:15:58 +0500

    Structures created

---
+++ client/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function NavBar() {
+    console.log('Navbar');
+
+    return (
+        <div className="nav-bar">
+            <h4>Navigation of LMS</h4>
+        </div>
+    );
+}
---
+++ client/src/Components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+import React from 'react';
+
+export default function Profile() {
+    console.log('Profile');
+
+    return (
+        <div>
+            <h1>Profile</h1>
+        </div>
+    );
+}
