d39235ca1607ec97837640afc96ffe73bf006166
ZainAsif767 <zainclasher2002@gmail.com>
2023-03-26 02:16:02 +0500

    deleted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,23 +7,18 @@ import {
   Lms,
   NavBar,
   Profile,
-  SignUp,
-  LogIn,
 } from "./Components";
 import "./App.css";
-// import { SignUp } from './Components/SignUp/SignUp';
+
 
 export default function App() {
   return (
     <div className="#">
       <CssBaseline />
       <NavBar />
-
-      <LogIn />
       <main className="#">
         <div className="#" />
         <Routes>
-          <Route path="/SignUp" element={<SignUp />}/>
           <Route exact path="/student/:id" element={<Student />} />
           <Route exact path="/teacher/:id" element={<Teacher />} />
           <Route exact path="/" element={<Lms />} />
