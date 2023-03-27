f3c7d21b0725d8778b9788004c7b6b5b3d7d0641
Zainasif767 <zainclasher2002@gmail.com>
2023-03-28 03:28:14 +0500

    Landing page

--- client/src/Components/Lms/Lms.jsx
+++ client/src/Components/Lms/Lms.jsx
@@ -1,22 +1,29 @@
 import React, { useEffect, useState } from "react";
 import LPNav from "./LPNav.jsx";
+import Welcome from "/Assets/Welcome.png"
 
 export default function Lms() {
   return (
     <main>
       <LPNav />
 
-      <div className="wlc">
-        <h1>Welcome to Acadoom</h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. deserunt
-          officiis ipsum
-        </p>
+      <div className="Welcome-container">
+        <div>
+          <h1>Welcome to Acadist</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. deserunt
+            officiis ipsum
+          </p>
+        </div>
+        <div>
+          <img src={Welcome} alt="Welcome" />
+        </div>
       </div>
       <hr />
       <div>
-        <h2>What is Acadoom?</h2>
+        <h2>What is Acadist?</h2>
       </div>
     </main>
   );
 }
+// how to add a gradient?
