2cc9801541bfa62f77d4d1a47885620159348382
ZainAsif767 <zainclasher2002@gmail.com>
2023-03-31 02:54:08 +0500

    fonts for Partners

--- client/src/Components/LandingPage/Partner/Partner.jsx
+++ client/src/Components/LandingPage/Partner/Partner.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faGoogle, faAmazon, faApple } from '@fortawesome/free-brands-svg-icons';
 import './Partner.css';
 
 export default function Partner() {
     return (
         <div className="partner-container">
             <h2 className='partner-text'>Our Partners</h2>
+            <div className="partner-icons">
+                <div className="font">
+                    <a href="/" target={"_blank"}>
+                        <FontAwesomeIcon icon={faAmazon} size="lg" />
+                    </a>
+
+                    <a href="/" target={"_blank"}>
+                        <FontAwesomeIcon icon={faGoogle} size="lg" />
+                    </a>
+                    <a href="/" target={"_blank"}>
+                        <FontAwesomeIcon icon={faApple} size="lg" />
+                    </a>
+                    <a href="/" target={"_blank"}>
+                        <FontAwesomeIcon icon={faTwitter} size="lg" />
+                    </a>
+                </div>
+            </div>
         </div>
     );
 }
