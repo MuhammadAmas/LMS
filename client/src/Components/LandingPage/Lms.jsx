4c6de8715af1da4e651830a9b86474e0fbde7d99
ZainAsif767 <zainclasher2002@gmail.com>
2023-03-31 00:31:21 +0500

    Feat: components

--- client/src/Components/LandingPage/Lms.jsx
+++ client/src/Components/LandingPage/Lms.jsx
@@ -1,59 +1,23 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import LPNav from "./NavBar/LPNav.jsx";
-import Divider from '@mui/material/Divider';
-
-import Hero from "/Assets/Hero.png"
-import "./Lms.css"
+import Hero from './Hero/Hero.jsx';
+import About from "./About/About.jsx";
+import Partner from "./Partner/Partner.jsx"
 import Footer from "./Footer/Footer.jsx";
-// import BackgroundAnimations from "../Background Animations/BackgroundAnimations.jsx";
+import Divider from '@mui/material/Divider';
+import "./Lms.css"
 
 export default function Lms() {
   return (
-    <main>
+    <>
       <LPNav />
-
-      <div className="hero-container">
-        <div className="hero-wrapper">
-          <h1 className="hero-text">Welcome to Acadist</h1>
-          <span className="hero-tag">
-            Expand your knowledge with our digital learning platform
-          </span>
-          <div className="buttons">
-            <Link to="/signin" className="login">
-              <button className="btn1">
-                Start Learning !
-              </button>
-            </Link>
-            <Link to="/signup" className="signup">
-              <button className="btn2">
-                Register for free
-              </button>
-            </Link>
-          </div>
-        </div>
-        <div className="hero-image-container">
-          <img src={Hero} alt="Welcome" className="hero-image" />
-        </div>
-      </div>
+      <Hero />
       <Divider variant="middle" />
-      <div className="what-container">
-        <h2 className="what-text">What is Acadist?</h2>
-        <ul className="list">
-          <li>Online platform for learning and teaching.</li>
-          <li>Offers courses on various subjects and topics.</li>
-          <li>Taught by experienced educators from around the world.</li>
-          <li>Accessible anytime, anywhere with an internet connection.</li>
-          <li>Tools for instructors to create and manage courses.</li>
-          <li>Start your journey to lifelong learning with Acadist.</li>
-        </ul>
-      </div>
+      <About />
       <Divider variant="middle" />
-      <div className="partner-container">
-        <h2>Partners</h2>
-      </div>
+      <Partner />
       <Footer />
+    </>
 
-    </main>
   );
 }
