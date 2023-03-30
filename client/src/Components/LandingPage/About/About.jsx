f49a20f823c0a64f6bd8869310ba2075d2f03079
ZainAsif767 <zainclasher2002@gmail.com>
2023-03-31 00:31:37 +0500

    About/What Section

---
+++ client/src/Components/LandingPage/About/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+    return (
+        <div className="what-container">
+            <h2 className="what-text">What is Acadist?</h2>
+            <ul className="list">
+                <li>Online platform for learning and teaching.</li>
+                <li>Offers courses on various subjects and topics.</li>
+                <li>Taught by experienced educators from around the world.</li>
+                <li>Accessible anytime, anywhere with an internet connection.</li>
+                <li>Tools for instructors to create and manage courses.</li>
+                <li>Start your journey to lifelong learning with Acadist.</li>
+            </ul>
+        </div>
+    );
+}
