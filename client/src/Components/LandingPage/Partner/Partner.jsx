e7b21ac2492995e0f628c4d84b20bfbea3d4a7bb
ZainAsif767 <zainclasher2002@gmail.com>
2023-04-01 02:28:57 +0500

    Partner complete !

--- client/src/Components/LandingPage/Partner/Partner.jsx
+++ client/src/Components/LandingPage/Partner/Partner.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faGoogle, faAmazon, faApple } from '@fortawesome/free-brands-svg-icons';
 import './Partner.css';
+import twitter from '/Assets/twitter.png'
+import apple from '/Assets/apple.png'
+import google from '/Assets/google.png'
+import facebook from '/Assets/facebook.png'
 
 export default function Partner() {
     return (
         <div className="partner-container">
             <h2 className='partner-text'>Our Partners</h2>
             <div className="partner-icons">
-                <div className="font">
-                    <a href="/" target={"_blank"}>
-                        <FontAwesomeIcon icon={faAmazon} size="lg" />
-                    </a>
-
-                    <a href="/" target={"_blank"}>
-                        <FontAwesomeIcon icon={faGoogle} size="lg" />
-                    </a>
-                    <a href="/" target={"_blank"}>
-                        <FontAwesomeIcon icon={faApple} size="lg" />
-                    </a>
-                    <a href="/" target={"_blank"}>
-                        <FontAwesomeIcon icon={faTwitter} size="lg" />
-                    </a>
+                <div>
+                    <img src={twitter} alt="Twitter" />
+                    <a href='/'><h4>Twitter</h4></a>
+                </div>
+                <div>
+                    <img src={facebook} alt="Facebook" />
+                    <a href='/'><h4>Facebook</h4></a>
+                </div>
+                <div>
+                    <img src={apple} alt="Apple" />
+                    <a href='/'><h4>Apple</h4></a>
+                </div>
+                <div>
+                    <img src={google} alt="Google" />
+                    <a href='/'> <h4>Google</h4></a>
                 </div>
             </div>
         </div>
