71c6192c1e3ece0b83d2c0b4983fddb8c3c2b11d
ZainAsif767 <zainclasher2002@gmail.com>
2023-04-06 23:59:37 +0500

    progress

--- client/src/Components/Student/Student.jsx
+++ client/src/Components/Student/Student.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import UdemyCourses from '../getUdemyCourses';
 
 
 export default function Student() {
 
     return (
         <div >
-            <h1>Student</h1>
+            <h1>Student!!!</h1>
+            <UdemyCourses />
         </div>
     );
 }
---
+++ client/src/Components/udemyApi.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config()
+
+// export const getUdemyCourses = async () => {
+//     // resonse_object.header("Access-Control-Allow-Origin", "*");
+//     // resonse_object.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     const response = await axios.get('https://www.udemy.com/api-2.0/courses/238934', {
+//         headers: {
+//             // Authorization: `Basic ${(`${clientId}:${clientSecret}`)}`,
+
+//             "Accept": "application/json, text/plain, */*",
+//             "Authorization": "Basic NmtKWThHSXl5dmlGMXcwOExUREVzbGZydUxwWlZqenljOXhTekpWajp4cEtqNk5aZjlpUXJLMDNQcGVRU2FQUGx1Y2s2UmNrY1JrZVNOZHh0dm1XUWRHQ1AwM3FQYXRTSm8xWWJSQUhEQ01qRFh0Vk1rbHdGdHZxcmhKcG1yQ1FHWENLRVhUSzdwR3FDTndxRk5qUVZDOEtaSGpiemE5a3Bib1ZZejhQcQ==",
+//             "Content-Type": "application/json",
+//             "Access-Control-Allow-Origin": "*"
+//         },
+//     });
+//     return response.data;
+// };
+
+const udemyKey = process.env.REACT_APP_UDEMY_KEY;
+const udemySecret = process.env.REACT_APP_UDEMY_SECRET;
+
+const apiUrl = 'https://www.udemy.com/api-2.0/courses/';
+
+export const getUdemyCourses = async () => {
+    try {
+        const response = await axios.get(apiUrl, {
+            headers: {
+                'Authorization': 'Bearer ' + udemySecret,
+                'X-Udemy-Client-Id': udemyKey,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+};
