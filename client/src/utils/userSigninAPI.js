5c232237990067b800b336065622d98593ce20d2
ZainAsif767 <zainclasher2002@gmail.com>
2023-04-19 03:39:50 +0500

    API for signin/up

---
+++ client/src/utils/userSigninAPI.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export default async function userSignin(method, url, data) {
+    const options = { method: method, url: url, data: data };
+    try {
+        const response = await axios.request(options);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
