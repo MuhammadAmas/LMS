b9747044916c80cb4b2d9467aee4708d7ff3080c
ZainAsif767 <zainclasher2002@gmail.com>
2023-04-29 18:21:05 +0500

    fixes

--- client/src/Components/Teacher/Teacher.jsx
+++ client/src/Components/Teacher/Teacher.jsx
@@ -9,12 +9,12 @@ export default function Student() {
     return (
         <div className='page-container'>
             <Nav />
-            
-            <CreateCourse/>
-            <h1>Created Course</h1>
+
+            <CreateCourse />
+            <h1>Created Courses</h1>
             <Courses />
 
-            <h1>All Course</h1>
+            <h1>All Courses</h1>
             <Divider />
             <Courses />
         </div>
