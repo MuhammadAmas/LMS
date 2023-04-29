df495747e1834154b1ee3601157a7a219852adec
muhammadamas <amaswaseem@gmail.com>
2023-04-29 12:51:44 +0500

    updated

--- client/src/Components/Teacher/NavLink.jsx
+++ client/src/Components/Teacher/NavLink.jsx
@@ -19,20 +19,6 @@ import {
 } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
-// const NavLink = ({ children }) => (
-//     <Link
-//         px={2}
-//         py={1}
-//         rounded={'md'}
-//         _hover={{
-//             textDecoration: 'none',
-//             bg: useColorModeValue('gray.200', 'gray.700'),
-//         }}
-//         href={'#'}>
-//         {children}
-//     </Link>
-// );
-
 export default function Nav() {
     const { colorMode, toggleColorMode } = useColorMode();
     const { isOpen, onOpen, onClose } = useDisclosure();
