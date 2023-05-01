c02a7c086c81ce1e7520433c024dcb6d985b6422
ZainAsif767 <zainclasher2002@gmail.com>
2023-05-02 00:05:42 +0500

    props in Courses

--- client/src/Components/Teacher/Teacher.jsx
+++ client/src/Components/Teacher/Teacher.jsx
@@ -6,17 +6,19 @@ import './Student.css'
 import CreateCourse from './CreateCourse';
 
 export default function Student() {
+    const user_id = localStorage.getItem("user_id") != null ? localStorage.getItem("user_id") : sessionStorage.getItem("user_id");
+
     return (
         <div className='page-container'>
             <Nav />
 
             <CreateCourse />
             <h1>Created Courses</h1>
-            <Courses />
+            <Courses userID={user_id} />
 
             <h1>All Courses</h1>
             <Divider />
-            <Courses />
+            <Courses userID={null} />
         </div>
     );
 }
