c0cc5758939e9bf7ae499ce22fc98a78416d69e3
MuhammadAmas <amaswaseem@gmail.com>
2023-07-08 19:00:51 +0500

    updated

--- client/src/Components/LandingPage/Hero/Hero.jsx
+++ client/src/Components/LandingPage/Hero/Hero.jsx
@@ -168,28 +168,6 @@ export default function Hero() {
                 </Flex> */}
             </Stack>
         </Container >
-        <div className="hero-container">
-            <div className="hero-wrapper">
-                <h1 className="hero-text">Welcome to Acadist</h1>
-                <span className="hero-tag">Expand your knowledge with our digital learning platform
-                </span>
-                <div className="buttons">
-                    <Link to="/signin" className="login">
-                        <button className="btn1">
-                            Start Learning !
-                        </button>
-                    </Link>
-                    <Link to="/signup" className="signup">
-                        <button className="btn2">
-                            Register for free
-                        </button>
-                    </Link>
-                </div>
-            </div>
-            <div className="hero-image-container">
-                <img src={HeroPic} alt="Welcome" className="hero-image" />
-            </div>
-        </div>
     );
 }
 
